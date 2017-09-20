20122595cd31f5275915eda82b021d066b8fa5be
Niki02 <31310288+Niki02@users.noreply.github.com>
2017-09-20 22:04:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,10 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 
 element.innerHTML='New Value';
+
+//move the image
+var img=document.getElementById('madi');
+madi.onclick=function () {
+    madi.style.marginLeft = '100px';
+    
+};
